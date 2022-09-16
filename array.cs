d320ba945d5a8014cd917af4e896b5c5ddd2802c
tarunnag <113530491+tarunnag@users.noreply.github.com>
2022-09-16 18:47:27 +0530

    Add files via upload

---
+++ array.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class array
+{
+    public static void Main()
+    {
+        Console.WriteLine("enter the length of array:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int[] c = new int[a];
+        for (int i = 0; i < a; i++)
+        {
+            Console.WriteLine("enter the the integer:");
+            c[i] = Convert.ToInt32(Console.ReadLine());
+            
+        }
+        Array.Sort(c);
+        Console.WriteLine(c[c.Length - 2]);
+    }
+}
